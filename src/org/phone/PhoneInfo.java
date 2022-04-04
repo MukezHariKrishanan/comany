d27b46b2af72d2c88837a08e4f731bcaa2d4b479
MukezHariKrishnan <mukeshappun@gmil.com>
2022-04-04 17:25:01 +0530

    company

---
+++ src/org/phone/PhoneInfo.java
@@ -0,0 +1,43 @@
+package org.phone;
+
+public class PhoneInfo {
+
+	private void phoneName() {
+		System.out.println("Iphone");
+
+	}
+	
+	private void phoneMieiNum() {
+		System.out.println("321654987123");
+
+	}
+	
+	private void Camera() {
+		System.out.println("12MP");
+
+	}
+	
+	private void storage() {
+		System.out.println("64GB");
+
+	}
+	private void osName() {
+		System.out.println("ios");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		//object creation - ClassName = new ClassName
+		
+		PhoneInfo PI = new PhoneInfo();
+				
+		//method call - objectName - methodName();
+		
+				PI.phoneName();
+				PI.phoneMieiNum();
+				PI.Camera();
+				PI.storage();
+				PI.osName();
+	}
+}
